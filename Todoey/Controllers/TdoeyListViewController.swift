3cffb9d9db85d3e57a92dbce54cd7ee8be37421f
Matteo Postorino <matteoposto84@macbook-pro-di-matteo.local>
2019-09-16 08:17:47 +0200

    Commit to MVC Design Pattern

--- Todoey/Controllers/TdoeyListViewController.swift
+++ Todoey/Controllers/TdoeyListViewController.swift
@@ -10,9 +10,10 @@ import UIKit
 
 class TdoeyListViewController: UITableViewController {
 
-    //Now array is usable initiallizig it as var
-    var itemArray = ["Sephiroth", "Cloud", "Squall", "Rinoa"]
+    //Now array is usable initiallizig it as va
     
+    var itemArray = [Item]()
+
     //Initializing Default User Settings to Store Data into our app dominio
     let defaults = UserDefaults.standard
     
@@ -20,17 +21,35 @@ class TdoeyListViewController: UITableViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        let newItem_1 = Item()
+        newItem_1.title = "Sephiroth"
+        itemArray.append(newItem_1)
+        
+        let newItem_2 = Item()
+        newItem_2.title = "Cloud"
+        itemArray.append(newItem_2)
+        
+        let newItem_3 = Item()
+        newItem_3.title = "Squall"
+        itemArray.append(newItem_3)
+        
+        let newItem_4 = Item()
+        newItem_4.title = "Rinoa"
+        itemArray.append(newItem_4)
+     
         //showing saved array into default stored data. Cast it as an array of strings.
         
         //Adding an if statement to check if data exists
-        if let items = defaults.array(forKey: "ToDoListArray") as? [String]{
+        if let items = defaults.array(forKey: "ToDoListArray") as? [Item]{
             itemArray = items
         }
+        
     }
     
     //Table view DataSource Methods
     //Telling Xcode to create N cells depending on array count
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
         return itemArray.count
     }
     
@@ -39,28 +58,60 @@ class TdoeyListViewController: UITableViewController {
         
         //selecting localTableview referring to local argument, not global.
         
+        //Checkmark is a property of the cell, so if that cell is dequed, checkmark will be kept in the same cell but value will be different -> It is necessary to associate checkmark with the cell+value itself.
+        
+        //We Use a model
+    
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
         
+        cell.textLabel?.text = item.title
+        
+        item.done == true ? (cell.accessoryType = .checkmark) : (cell.accessoryType = .none)
+        
+        /*
+        if item.done == true {
+            cell.accessoryType = .checkmark
+        }
+        else
+        {
+            cell.accessoryType = .none
+        }
+        */
         return cell //UItableViewCell has to be returned
     }
     
     //Setting table view func to tell delegate row was selected
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //print(itemArray[indexPath.row])
+ 
+        //Setting cell property so as user can change it depending on actual status (true ->false; false->true)
+        
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        //FOLLOWING CODE DOES THE SAME THING
+        /*
+        if itemArray[indexPath.row].done == false {
+            itemArray[indexPath.row].done = true
+        }
+        else {
+            itemArray[indexPath.row].done = false
+        }
+        */
+        
+        tableView.reloadData()
         
         //Cell ogject for managing its features
-        let myCell:UITableViewCell = tableView.cellForRow(at: indexPath)!
+        //let myCell:UITableViewCell = tableView.cellForRow(at: indexPath)!
         
-        if myCell.accessoryType == .checkmark
-        {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }
-        else if myCell.accessoryType == .none
-        {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        //if myCell.accessoryType == .checkmark
+        //{
+        //    tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        //}
+        //else if myCell.accessoryType == .none
+        //{
+        //    tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        //}
         //Deselecting row (background colour grey) when pushed
         tableView.deselectRow(at: indexPath, animated: true)
     }
@@ -80,10 +131,13 @@ class TdoeyListViewController: UITableViewController {
             //What happens when Add Item button is pressed
             
             //Appending new element to array
-            self.itemArray.append(textField.text!)
+            let newItem = Item()
+            newItem.title = textField.text!
+            
+            self.itemArray.append(newItem)
             
             //Saving added element to default settings
-            //You can retrieve all elements saved by using set key value
+            //You can retrieve all elements saved by using set key command
             //We need ID of Sandbox and Simulator where data is stored -> AppDelegate.swift
             
             self.defaults.set(self.itemArray, forKey: "ToDoListArray")
---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Matteo Postorino on 03/09/2019.
+//  Copyright © 2019 Matteo Postorino. All rights reserved.
+//
+
+import Foundation
+
+//creating class to solve bug relating to checkmark
+class Item{
+    
+    var title:String = ""
+    var done:Bool = false
+    
+}
